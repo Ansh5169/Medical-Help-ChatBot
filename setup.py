e38caeb66d9477b2216d61bf605a54a89c8cd406
Ansh Gupta <guptaag2004@gmail.com>
2025-08-23 21:44:21 +0530

    folder structure

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name="Generative AI Project",
+    version="0.0.0",
+    author="Ansh",
+    packages=find_packages(),
+    install_requires=[]
+)
